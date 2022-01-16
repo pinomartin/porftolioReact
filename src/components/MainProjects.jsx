907b5b129ed149a88eadccc92e097f3eef3475a3
pinomartin <martin91pino@gmail.com>
2022-01-16 16:20:07 -0300

    changes on skill imgs

--- src/components/MainProjects.jsx
+++ src/components/MainProjects.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import projectsData from "../data/projects";
-import { Skill } from "./Skill";
+// import projectsData from "../data/projects";
+// import { Skill } from "./Skill";
 
 export const MainProjects = () => {
   return (
